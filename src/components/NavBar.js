da17dd52be357ba4825422dda9981fddcffc20ba
Molly Ann McDonough <mcdonough.mollya@gmail.com>
2022-02-24 13:05:59 -0500

    commit tree 1540e034d92db3551a2b827d655c04d429809a35

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,13 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const linksJsx = links.map(link => {
+    return (
+      <a key={link} href={`#${link}`}>{link}</a>
+    );
+  })
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{linksJsx}</nav>;
 }
 
 export default NavBar;
